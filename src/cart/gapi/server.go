f6c394a63212a3b9aee0a25168a3ab490f69b8c8
Arthur Rubchenko <8872307@gmail.com>
2023-03-21 14:17:37 +0200

    cart: add health check

--- src/cart/gapi/server.go
+++ src/cart/gapi/server.go
@@ -3,10 +3,12 @@ package gapi
 import (
 	cartv1 "github.com/Arthur199212/microservices-demo/gen/services/cart/v1"
 	"github.com/Arthur199212/microservices-demo/src/cart/db"
+	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 type Server struct {
 	cartv1.UnimplementedCartServiceServer
+	grpc_health_v1.UnimplementedHealthServer
 	cartDB db.CartDB
 }
 
